ce99f05ca65a79b7656525a2c0b8be4815f936bd
YanaSmirnova <smirnova.yana@gmail.com>
2015-01-29 21:40:00 +1100

    Random number range = number elements in the array

--- app/src/main/java/com/yanasmirnova/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/yanasmirnova/funfacts/FunFactsActivity.java
@@ -37,10 +37,11 @@ public class FunFactsActivity extends ActionBarActivity {
                 "The state of Florida is bigger than England.",
                 "Some penguins can leap 2-3 meters out of the water.",
                 "On average, it takes 66 days to form a new habit.",
-                "Mammoths still walked the earth when the Great Pyramid was being built." };
+                "Mammoths still walked the earth when the Great Pyramid was being built.",
+                "Treehouse is not actually in a tree."};
             // Randomly select a fact
             Random randomGenerator = new Random(); //Construct a new Random number generator
-            int randomNumber = randomGenerator.nextInt(10);
+            int randomNumber = randomGenerator.nextInt(facts.length);
             fact = facts[randomNumber];
             // Update the label with our dynamic fact
             factLabel.setText(fact);
