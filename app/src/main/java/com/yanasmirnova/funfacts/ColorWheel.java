f1720c79e1d63ba13800bec17679505e9715c51b
YanaSmirnova <smirnova.yana@gmail.com>
2015-01-29 22:57:27 +1100

    Separate class for keeping array of colors and choose from them

---
+++ app/src/main/java/com/yanasmirnova/funfacts/ColorWheel.java
@@ -0,0 +1,33 @@
+package com.yanasmirnova.funfacts;
+
+import java.util.Random;
+
+/**
+ * Class to manage background color changes.
+ */
+public class ColorWheel {
+    // Member variables
+    public String mColors[] = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    // Methods
+    public String getColor() {
+        // Randomly select a color
+        Random randomGenerator = new Random(); //Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+        return mColors[randomNumber];
+    }
+}
