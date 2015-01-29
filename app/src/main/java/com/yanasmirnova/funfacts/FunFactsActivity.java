74d20cb87a922d0297202acb4df4b0e18ed6beba
YanaSmirnova <smirnova.yana@gmail.com>
2015-01-29 21:30:34 +1100

    Pulls now facts from an Array based on random index.

--- app/src/main/java/com/yanasmirnova/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/yanasmirnova/funfacts/FunFactsActivity.java
@@ -25,30 +25,23 @@ public class FunFactsActivity extends ActionBarActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-        // The button was clicked, so update the fact label with a new fact
+            // The button was clicked, so update the fact label with a new fact
             String fact = "";
+            String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built." };
             // Randomly select a fact
             Random randomGenerator = new Random(); //Construct a new Random number generator
-            int randomNumber = randomGenerator.nextInt(3);
-
-            /* Convert the random number to a text fact
-                0 = Ants stretch when they wake up in the morning.
-                1 = Ostriches can run faster than horses.
-                2 = Olympic gold medals are made mostly of silver.
-             */
-            if (randomNumber == 0) {
-                fact = "Ants stretch when they wake up in the morning.";
-            }
-            else if (randomNumber == 1) {
-                fact = "Ostriches can run faster than horses.";
-            }
-            else if (randomNumber == 2) {
-                fact = "Olympic gold medals are made mostly of silver.";
-            }
-            else {
-                fact = "Sorry, there was an error.";
-            }
-
+            int randomNumber = randomGenerator.nextInt(10);
+            fact = facts[randomNumber];
             // Update the label with our dynamic fact
             factLabel.setText(fact);
             }
